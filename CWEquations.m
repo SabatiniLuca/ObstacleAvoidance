59e90c41aa1755e2f9d6137c13ffc4d9cf089fe3
Luca Sabatini <s1099243@studenti.univpm.it>
2022-02-08 16:04:18 +0100

    Implementazione CWE

--- CWEquations.m
+++ CWEquations.m
@@ -7,26 +7,28 @@
 %     int(-(n^2)*z ,z);];
 gamma= 3.986e14;%cost grav
 a=400000;%altitudine esemplificativa
-n=sqrt(gamma/(a^3));
+delta=sqrt(gamma/(a^3));
 %posizione iniziale
-P0=[0;
-    0;
-    a;];
+P0=[4235;345;a;];
 % definizione delle equazioni in forma chiusa
-QuatPosition = @(P0)   [(4-3*cos(n*t))*P0(1,1) + (sin(n*t)/n)*diff(P0(1,1)) + (2/n)*(1-cos(n*t))*diff(P0(2,1));
-                        6*(sin(n*t)-n*t)*P0(1,1) + P0(2,1) - (2/n)*(1-cos(n*t))*diff(P0(1,1)) + ((4*sin(n*t)-(3*n*t))/n)*diff(P0(2,1));
-                        P0(3,1)*cos(n*t) + (diff(P0(3,1))/n)*sin(n*t);];
+
+QuatPosition = @(P0,t)   [(4-3*cos(delta*t))*P0(1,1) + (sin(delta*t)/delta)*diff(P0(1,1)) + (2/delta)*(1-cos(delta*t))*diff(P0(2,1));
+                        6*(sin(delta*t)-delta*t)*P0(1,1) + P0(2,1) - (2/delta)*(1-cos(delta*t))*diff(P0(1,1)) + ((4*sin(delta*t)-(3*delta*t))/delta)*diff(P0(2,1));
+                        P0(3,1)*cos(delta*t) + (diff(P0(3,1))/delta)*sin(delta*t);];
 %condizioni operative
 tf= 50;
 N=100;
 h=tf/N;%passo numerico
 
 %metodo numerico
-Phi_fEul  = @(y,h) y + h*QuatPosition(y);
+Phi_fEul  = @(y,h,n) y + h*QuatPosition(y,n);
 
-p_fEul(1)=0;
+
+p_fEul(1)=QuatPosition(P0,1);
+
 %calcolo
 for n=1:N
-    p_fEul(n+1) = Phi_fEul(p_fEul(n),h);
+    p_fEul(n+1) = Phi_fEul(p_fEul(n),h,n);
+    P0=p_fEul(n+1);
 end
 plot(h*(0:N),p_fEul,'magenta')
