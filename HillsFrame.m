cc25e914b1e7fc6d497a899ef9b017659fe4f30b
Luca Sabatini <s1099243@studenti.univpm.it>
2022-02-24 18:28:10 +0100

    Prova di implementazione della prima parte delle tre equazioni di hill

--- HillsFrame.m
+++ HillsFrame.m
@@ -1,48 +1,17 @@
-%Definisco le equazioni di Hill. Queste equazioni sono le accelerazioni nel
-%frame di Hill per i tre assi x,y,z.
+%% Scrivo le equazioni di Hill aggiungendo tre variabili ausiliarie
+h=100;
+Fx= 328;
+mc=2392832;
+y(1)= 0;
+a(1)= 1;
 
-syms x y z;%Hill's positions
+for k= 1:100
+    y(k+1)=y(k)+(1/h)*a(k);
+    a(k+1)= a(k) + (Fx/mc)*(1/h);
+end
 
-
-%mc is the mass of the spacecraft
-% F x,y,x is the force in each direction
-% w0 is the orbital angular velocity
-%v is the velocity in each direction
-Vlvh=[diff(x);
-      diff(y);
-      diff(z);];
-%this was the definition of the VELOCITY in the LVLH frame
-
-%is the thrusters force which is given by multiplying the matrix of
-%rotation of the spacecraft and the thrust force expressed in bodyframe
-% Note: see Body Frame definition in "Definizioni"
-% Fthr=Rb*Fb;
-%this was the definition of the Thrusters Force in the LVLH frame
-
-
-% P is the density of the atmosphere
-% V is the orbital Velocity of the spacecraft
-% S is the frontal section
-%Cd is the drag coefficient
-% Flvh= [Fthr(1,1)-(1/2)*P*Vlvh^2*S*Cd;
-%         Fthr(2,1);
-%         Fthr(3,1);];
-%this was the definition of the FORCE in the LVLH frame
-P=5474.89;%air pressure at 20 km
-Vlvh=[7666;
-        0;
-        0;];%example of velocity(ISS velocity)
-syms S Cd;
-Flvh= [-(1/2)*P*Vlvh.^2*S*Cd;
-        0;
-        0;];
-mc=7080;
-w0=Vlvh(1,1)*pi*20000;
-Alvh= [(Flvh(1,1)/mc)+(2*w0*Vlvh(3,1));
-       (Flvh(2,1)/mc)-(w0*Vlvh(2,1));
-        (Flvh(3,1)/mc)-(2*w0*Vlvh(1,1))+(3*w0^2*z);];
-%this was the definition of the acceleration in the LVLH frame
-
-
+figure(1)
+plot(y)
+title('legge del moto')
 
 
